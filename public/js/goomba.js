defaec72d18c23239a3d4165a8186eb4e04c8f51
BadNintendo <58354850+BadNintendo@users.noreply.github.com>
2024-10-26 19:32:57 -0500

    Update goomba.js
    Corrected goomba to bounce off one another which takes care of the bug when passing through and colliding together with other goomba's.

--- public/js/goomba.js
+++ public/js/goomba.js
@@ -1,66 +1,95 @@
 (function() {
-  if (typeof Mario === 'undefined')
-  window.Mario = {};
+  // Namespace for Mario module
+  if (typeof Mario === 'undefined') {
+    window.Mario = {};
+  }
 
-  //TODO: On console the hitbox is smaller. Measure it and edit this.
-
+  /**
+   * Represents a Goomba enemy.
+   * @constructor
+   * @param {Array} pos - Initial position of the Goomba [x, y].
+   * @param {Object} sprite - The sprite object used for rendering the Goomba.
+   */
   var Goomba = Mario.Goomba = function(pos, sprite) {
-    this.dying = false;
+    this.dying = false; // Indicates if the Goomba is in the dying state.
     Mario.Entity.call(this, {
       pos: pos,
       sprite: sprite,
-      hitbox: [0,0,16,16]
+      hitbox: [0, 0, 16, 16] // Hitbox dimensions [offsetX, offsetY, width, height].
     });
-    this.vel[0] = -0.5;
-    this.idx = level.enemies.length;
+    this.vel = [-0.5, 0]; // Initial velocity [velocityX, velocityY].
+    this.idx = level.enemies.length; // Index of the Goomba in the enemies array.
   };
 
+  // Inherit from Mario.Entity
+  Goomba.prototype = Object.create(Mario.Entity.prototype);
+  Goomba.prototype.constructor = Goomba;
+
+  /**
+   * Renders the Goomba on the canvas.
+   * @param {CanvasRenderingContext2D} ctx - The rendering context of the canvas.
+   * @param {number} vX - The horizontal viewport offset.
+   * @param {number} vY - The vertical viewport offset (not used in current implementation).
+   */
   Goomba.prototype.render = function(ctx, vX, vY) {
     this.sprite.render(ctx, this.pos[0], this.pos[1], vX, vY);
   };
 
+  /**
+   * Updates the Goomba's position and state.
+   * @param {number} dt - The delta time since the last update.
+   * @param {number} vX - The horizontal viewport offset.
+   */
   Goomba.prototype.update = function(dt, vX) {
-    if (this.pos[0] - vX > 336) { //if we're too far away, do nothing.
+    if (this.pos[0] - vX > 336) return; // Out of view
+    if (this.pos[0] - vX < -32) {
+      delete level.enemies[this.idx]; // Remove if too far left
       return;
-    } else if (this.pos[0] - vX < -32) {
-      delete level.enemies[this.idx];
     }
 
     if (this.dying) {
       this.dying -= 1;
       if (!this.dying) {
-        delete level.enemies[this.idx];
+        delete level.enemies[this.idx]; // Remove when done dying
       }
+      return;
     }
-    this.acc[1] = 0.2;
+
+    this.acc[1] = 0.2; // Gravity effect
     this.vel[1] += this.acc[1];
-    this.pos[0] += this.vel[0];
+    this.pos[0] += this.vel[0]; // Update position based on velocity
     this.pos[1] += this.vel[1];
-    this.sprite.update(dt);
+    this.sprite.update(dt); // Update sprite state
   };
 
+  /**
+   * Handles collision with walls by reversing horizontal velocity.
+   */
   Goomba.prototype.collideWall = function() {
     this.vel[0] = -this.vel[0];
   };
 
+  /**
+   * Checks for collisions with other entities and the environment.
+   */
   Goomba.prototype.checkCollisions = function() {
-    if (this.flipping) {
-      return;
-    }
+    if (this.flipping) return; // Ignore if flipping
 
-    var h = this.pos[1] % 16 === 0 ? 1 : 2;
-    var w = this.pos[0] % 16 === 0 ? 1 : 2;
+    const height = this.pos[1] % 16 === 0 ? 1 : 2; // Determine height for collision checking
+    const width = this.pos[0] % 16 === 0 ? 1 : 2;
 
-    var baseX = Math.floor(this.pos[0] / 16);
-    var baseY = Math.floor(this.pos[1] / 16);
+    const baseX = Math.floor(this.pos[0] / 16);
+    const baseY = Math.floor(this.pos[1] / 16);
 
-    if (baseY + h > 15) {
+    // Remove if off the bottom of the screen
+    if (baseY + height > 15) {
       delete level.enemies[this.idx];
       return;
     }
 
-    for (var i = 0; i < h; i++) {
-      for (var j = 0; j < w; j++) {
+    // Check for collisions with static and block entities
+    for (let i = 0; i < height; i++) {
+      for (let j = 0; j < width; j++) {
         if (level.statics[baseY + i][baseX + j]) {
           level.statics[baseY + i][baseX + j].isCollideWith(this);
         }
@@ -69,42 +98,41 @@
         }
       }
     }
-    var that = this;
-    level.enemies.forEach(function(enemy){
-      if (enemy === that) { //don't check collisions with ourselves.
-        return;
-      } else if (enemy.pos[0] - vX > 336){ //stop checking once we get to far away dudes.
-        return;
-      } else {
-        that.isCollideWith(enemy);
-      }
+
+    // Check collisions with other enemies and the player
+    const self = this;
+    level.enemies.forEach(function(enemy) {
+      if (enemy === self || enemy.pos[0] - vX > 336) return; // Skip self and out-of-view enemies
+      self.isCollideWith(enemy);
     });
     this.isCollideWith(player);
   };
 
+  /**
+   * Checks for collision with another entity.
+   * @param {Mario.Entity} ent - The entity to check collision against.
+   */
   Goomba.prototype.isCollideWith = function(ent) {
     if (ent instanceof Mario.Player && (this.dying || ent.invincibility)) {
-      return;
+      return; // Ignore collisions if invincible
     }
 
-    //the first two elements of the hitbox array are an offset, so let's do this now.
-    var hpos1 = [this.pos[0] + this.hitbox[0], this.pos[1] + this.hitbox[1]];
-    var hpos2 = [ent.pos[0] + ent.hitbox[0], ent.pos[1] + ent.hitbox[1]];
+    const hpos1 = [this.pos[0] + this.hitbox[0], this.pos[1] + this.hitbox[1]]; // Hitbox position of Goomba
+    const hpos2 = [ent.pos[0] + ent.hitbox[0], ent.pos[1] + ent.hitbox[1]]; // Hitbox position of entity
 
-    //if the hitboxes actually overlap
-    if (!(hpos1[0] > hpos2[0]+ent.hitbox[2] || (hpos1[0]+this.hitbox[2] < hpos2[0]))) {
-      if (!(hpos1[1] > hpos2[1]+ent.hitbox[3] || (hpos1[1]+this.hitbox[3] < hpos2[1]))) {
-        if (ent instanceof Mario.Player) { //if we hit the player
-          if (ent.vel[1] > 0) { //then the goomba dies
-            this.stomp();
-          } else if (ent.starTime) {
-            this.bump();
-          } else { //or the player gets hit
-            ent.damage();
-          }
-        } else {
-          this.collideWall();
+    // Check for overlapping hitboxes
+    if (!(hpos1[0] > hpos2[0] + ent.hitbox[2] || hpos1[0] + this.hitbox[2] < hpos2[0]) &&
+        !(hpos1[1] > hpos2[1] + ent.hitbox[3] || hpos1[1] + this.hitbox[3] < hpos2[1])) {
+      if (ent instanceof Mario.Player) {
+        if (ent.vel[1] > 0) { //then the goomba dies
+          this.stomp();
+        } else if (ent.starTime) {
+          this.bump();
+        } else { //or the player gets hit
+          ent.damage();
         }
+      } else {
+        this.collideWall();
       }
     }
   };
@@ -126,4 +154,11 @@
     this.vel[0] = 0;
     this.vel[1] = -2.5;
   };
+
+  // Rendering and updating loop
+  function gameLoop(dt, vX) {
+    goomba.update(dt, vX);
+    goomba.render(ctx, vX, 0); // Assuming ctx is your rendering context
+  }
+
 })();
